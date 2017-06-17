3ee079407723d1bf8d92b234bf77340514bf1a41
frafelcp <felixcastro1993@gmail.com>
2017-06-16 20:02:29 -0500

    arreglar el error que nos arroja al ejectura la prueba

---
+++ tests/Unit/NoteTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\BrowserKitTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Note;
+
+class NoteTest extends BrowserKitTestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testNote()
+    {
+        Note::create(['note' => 'my first note']);
+        $this->visit('notes')
+    		->see('my first note');
+    }
+}
